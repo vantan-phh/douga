da2c4114b94cb800c8934d1ed5f61b94eaa93282
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-02-20 17:21:57 +0900

    スペルミス

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,15 +1,8 @@
 const router = require("express").Router();
-const socket = require("socket.io");
 const server = require("../../app");
-
-let io = socket(server);
 
 router.get("/", (req, res) => {
   res.render("home");
-});
-
-io.sockets.on("connection", (socket) => {
-  console.log("a");
 });
 
 module.exports = router;
